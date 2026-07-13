4701af89f89d0a53c1e8be0069012b01b9894193
GaneshSharma31 <ganeshsharma31072007@gmail.com>
2026-07-13 12:05:47 +0530

    added nterms.cpp

--- nterms.cpp
+++ nterms.cpp
@@ -1,1 +1,13 @@
 # include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout << " enter a number\n";
+    cin >> n;
+    for ( int i=1 ; i<=n ; i++){
+          sum = sum + i;
+
+    }
+    cout << " sum of n terms is : " << sum << endl;
+    return 0;
+}
