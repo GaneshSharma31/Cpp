25401ced9a85c2af8bd070fe8f819edef6ffc831
GaneshSharma31 <ganeshsharma31072007@gmail.com>
2026-07-19 09:54:52 +0530

    using function we reduces redundancy

--- function.cpp
+++ function.cpp
@@ -8,8 +8,22 @@ int factorial(int n){
     }
 return fact;
 }
+int sum( int n){
+    int s=0;
+    for (int i=1;i<=n ;i++){
+        s +=i;
+    }
+    return s;
+}
 int main (){
-    int result = factorial (5);
-    cout << result << endl;
+    int n1,n2;
+    cout <<"enter n1 to find the factorial of a number";
+    cin >> n1;
+    cout << "enter n2 to find the sum of a number";
+    cin >> n2;
+    int factorialresult = factorial (n1);
+    int sumresult = sum (n2);
+    cout << factorialresult << endl;
+    cout << sumresult << endl;
     return 0 ;
 }
