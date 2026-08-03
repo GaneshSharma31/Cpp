2e87cb276ff3f94b5bb89098438ae24e6145b920
GaneshSharma31 <ganeshsharma31072007@gmail.com>
2026-08-03 17:30:32 +0530

    add diamond_pattern

---
+++ diamond_pattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    for (int i=0;i<n;i++){           // number of line of pattern
+        for (int j=0;j<n-i-1;j++){   // space
+            cout << " ";
+        }
+        for (int j=0;j<2*i+1;j++){   // stars
+            cout << "*";
+        }
+        
+        cout << endl;
+
+        
+    }
+    int k=3;
+    for (int i=0;i<n;i++){                   // number of line of pattern
+        for (int j=0;j<=i;j++){               // space
+            cout << " "; 
+        }
+        for (int j= 0;j<(2*k-1)-2*i;j++){    // stars
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
