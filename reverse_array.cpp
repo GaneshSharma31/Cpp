412989126b704489f3acb1f6e226702da8a0bcdc
GaneshSharma31 <ganeshsharma31072007@gmail.com>
2026-08-07 13:17:06 +0530

    add reverse array

---
+++ reverse_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void reverse_array (int arr[],int size){
+    int start =0 ,end = size -1;
+    while (start < end){
+        swap (arr[start],arr[end]);
+        start ++ ,end--;
+    }
+}
+int main (){
+    int arr[]={2,1,4,5,8,9};
+    int size= 6;
+    reverse_array (arr,size);
+    cout << "{";
+    for (int i=0;i<size;i++){   
+        
+        cout <<  arr[i]  ;
+        if(i<size-1){
+        cout << ",";}
+
+    }
+    cout << "}" << endl;
+    return 0;
+}
+
+ 
