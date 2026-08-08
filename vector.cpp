b042cee7a9e14a1e2757b0d3003d21a8d049aadf
GaneshSharma31 <ganeshsharma31072007@gmail.com>
2026-08-08 23:07:10 +0530

    add basic vector

---
+++ vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <int> v={23,1,3,4,5};
+    for(int x :v){
+        cout << x << " ";
+        
+    }
+    cout << endl;
+    return 0; 
+
+
+}
